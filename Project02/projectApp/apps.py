370578f53910bab33ef1c51905688417b0c52e3f
Diego Zavala <diezavala@csumb.edu>
2024-10-02 21:03:08 -0700

    Restarted project. Django app built with Docker, hosted on Heroku

--- Project02/projectApp/apps.py
+++ Project02/projectApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class ProjectappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'projectApp'
