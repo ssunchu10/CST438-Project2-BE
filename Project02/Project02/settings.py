089aa10f0448a51174f6ff4f4ea880fff069617f
Diego Zavala <diezavala@csumb.edu>
2024-10-07 18:51:15 -0700

    trying to connect db

--- Project02/Project02/settings.py
+++ Project02/Project02/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Project02.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gx8ww0d2h1x95tuu',  
+        'USER': 'er22dqpm3b2f5lk5',  
+        'PASSWORD': 'et9rd4br1jvdnjdj', 
+        'HOST': 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 
+        'PORT': '3306',  
     }
 }
 
